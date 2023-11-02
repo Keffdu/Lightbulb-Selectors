424df704ab07d825dda8eeb414146a00527a26eb
Kevin Dutzy <kddutz15@gmail.com>
2023-11-02 13:34:59 -0700

    complete project, started but need to finish challenge puzzle

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Write your code here
+let nodeLights = document.querySelectorAll(".lightbulb")
+let subtitle = document.querySelector(".subtitle")
+
+let count = 0
+let lights = [...nodeLights]
+
+
+lights.map((light) => {
+    light.addEventListener("mouseover", () => {
+        // count++
+        light.classList.toggle("active")
+        // count === 1 ? subtitle.textContent = `You've clicked the lightbulbs ${count} time` : subtitle.textContent = `You've clicked the lightbulbs ${count} times`
+    })
+    light.addEventListener("mouseout", () => {
+        // count++
+        light.classList.toggle("active")
+        // count === 1 ? subtitle.textContent = `You've clicked the lightbulbs ${count} time` : subtitle.textContent = `You've clicked the lightbulbs ${count} times`
+    })
+    // light.addEventListener("click", () => {
+    //     // count++
+    //     let lightId = parseInt(light.id[light.id.length - 1])
+    //     let lightTwo = lightId + 2
+    //     let lightThree = lightId + 4
+    //     light.classList.toggle("active")
+    //     lights[lightTwo].classList.toggle("active")
+    //     lights[lightThree].classList.toggle("active")
+    //     // count === 1 ? subtitle.textContent = `You've clicked the lightbulbs ${count} time` : subtitle.textContent = `You've clicked the lightbulbs ${count} times`
+    // })
+})
+
+// console.log(lights[lights.length - 1])
